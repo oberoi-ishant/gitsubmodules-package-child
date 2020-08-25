8643c3bf37ecce21e7dd7f3baaecbeb443a2b832
ishant.oberoi <ishant.oberoi@nutanix.com>
2020-08-25 15:10:47 +0530

    Adding styles and quotes

--- src/MyPage.js
+++ src/MyPage.js
@@ -4,9 +4,21 @@ class MyPage extends React.Component {
   render () {
     return (
       <div>
-        <div>Welcome to MyPage</div>
-        <div>Hello Page</div>
-        <div>Name: { this.props.name }</div>
+        <div class="mypage-heading__box">
+          <h3 class="mypage-heading__text heading-secondary">
+            <span class="mypage-heading__span">Game of Thrones: Quotes</span>
+          </h3>
+        </div>
+        <h3 class="u-margin-bottom-medium">Available on: { this.props.parent }</h3>
+        <p class="text u-margin-bottom-medium">
+          "Chaos isn’t a pit. Chaos is a ladder" - Lord Baelish
+        </p>
+        <p class="text u-margin-bottom-medium">
+        "The night is dark and full of terrors" - Melisandre
+        </p>
+        <p class="text u-margin-bottom-medium">
+        "Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you" - Tyrion Lannister
+        </p>
       </div>
     );
   }
